c25a31302d37c42d483449e169ea20bd38461bbe
Drive <drive8129@gmail.com>
2025-09-04 21:20:15 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'gateway.pinata.cloud',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   devIndicators: {
