1b3b10b7ac194d77c730266272d1154b8e7d1ed8
Your Name <drive8129@gmail.com>
2025-09-07 00:41:47 +0000

    Try fixing this error: `Build Error: Unknown module type. Error source:

--- next.config.ts
+++ next.config.ts
@@ -35,22 +35,7 @@ const nextConfig: NextConfig = {
     allowedDevOrigins: [
       '*.cloudworkstations.dev',
     ]
-  },
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          publicPath: '/_next/static/sounds/',
-          outputPath: 'static/sounds/',
-        },
-      },
-    });
-
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
